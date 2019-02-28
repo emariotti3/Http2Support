86928827e50ec8e9910310fc40a41edc35a1472a
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-02-28 19:16:55 -0300

    Added basic grid with Cards.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './components/Home/Home';
+import Album from './components/Home/Album';
 
 import './App.css';
 
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Album />
       </div>
     );
   }
