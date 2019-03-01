94102a849cba984488be4f79d5dd41f257d01d40
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-03-01 02:50:49 -0300

    Added basic style for issue cards.

--- src/components/Home/Album.js
+++ src/components/Home/Album.js
@@ -1,128 +1,110 @@
 import React from 'react';
-import classNames from 'classnames';
+import styles from './styles.css';
+import {MuiThemeProvider, createMuiTheme} from "@material-ui/core/styles";
+import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import { Card, CardMedia, CardContent, CardActions, } from '@material-ui/core';
-
-const styles = theme => ({
-  appBar: {
-    position: 'relative',
-  },
-  icon: {
-    marginRight: theme.spacing.unit * 2,
-  },
-  heroUnit: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  heroContent: {
-    maxWidth: 600,
-    margin: '0 auto',
-    padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
-  },
-  heroButtons: {
-    marginTop: theme.spacing.unit * 4,
-  },
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-      width: 1100,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  cardGrid: {
-    padding: `${theme.spacing.unit * 8}px 0`,
-  },
-  card: {
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing.unit * 6,
-  },
-});
-
-
-
+import red from "@material-ui/core/colors/red";
+import yellow from "@material-ui/core/colors/yellow";
+import { Button, } from '@material-ui/core';
 
 export default class Album extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      client : undefined
-    };
+      cases : []
+    };  
   }
 
-  componentDidMount(){
-    //fetch
-    this.setState({ client: undefined })
+  componentDidMount() {
+
+    fetch('https://jsonplaceholder.typicode.com/posts')	
+      .then(response => response.json())	
+      .then(result => {	
+        this.setState( { cases: [{
+          "id": 1234,
+          "priority": "MEDIUM",
+          "title": "A not so important ticket.",
+          "description": "No one cares.",
+          "trackId": 10303456,
+          "clientId": 595059,
+          "logo": null,
+          "caseStatus": "Assingned"
+        },
+        {
+          "id": 2233,
+          "priority": "HIGH",
+          "title": "A very important ticket.",
+          "description": "Please do something fast.",
+          "trackId": 12312322,
+          "clientId": 888888,
+          "logo": null,
+          "caseStatus": "Assingned"
+        }]
+       });	
+      });
   }
 
   render() {
-    const cases = [{
-      "id": 1234,
-      "priority": "Medium",
-      "title": "A not so important ticket.",
-      "description": "No one cares.",
-      "trackId": 10303456,
-      "clientId": 595059,
-      "logo": null,
-      "caseStatus": "Assingned"
-    },
-    {
-      "id": 2233,
-      "priority": "High",
-      "title": "A very important ticket.",
-      "description": "Please do something.",
-      "trackId": 12312322,
-      "clientId": 888888,
-      "logo": null,
-      "caseStatus": "Assingned"
-    }];
-    
+    const { classes } = this.props;
+
+    const priorities = { "HIGH": "secondary" , "MEDIUM": "primary" }
+
+    const theme = createMuiTheme({
+      palette: {
+        primary: yellow, 
+        secondary: red
+      },
+      typography: {
+        useNextVariants: true
+      }
+    });
+
     return (
       <div>
-        <Grid container spacing={40}>
-          {cases.map(card => (
-            <Grid item key={card} sm={6} md={4} lg={3}>
-              <Card className="card">
-                <CardMedia
-                  className="cardMedia"
-                  image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                  title="Image title"
-                />
-                <CardContent className="cardContent">
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small" color="primary">
-                    View
-                  </Button>
-                  <Button size="small" color="primary">
-                    Edit
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+        <Paper className="gridContainer" elevation={1}>
+          <Grid container spacing={24}>
+            {this.state.cases.map(card => (
+              <Grid item key={card} xs={3}>
+                <Paper className="card" >
+                    <div className="priorityLabel">
+                      <MuiThemeProvider theme={theme}>
+                        <Button 
+                          variant="contained" 
+                          center="left" 
+                          color = { priorities[card.priority.toUpperCase()] } 
+                          disableRipple
+                        >
+                          {card.priority.toUpperCase()} 
+                        </Button>
+                      </MuiThemeProvider>
+                    </div>
+
+                    <Typography variant="h5" colorTextSecondary className="cardHeader" gutterBottom="false">
+                      {card.title}
+                    </Typography>
+                    
+                    <Divider variant="middle" />
+                    
+                    <Typography color="textSecondary">
+                      {card.description}
+                    </Typography>
+                  
+                    <Typography align="center" className="cardStatus" color="textSecondary" gutterBottom>
+                      Status: {card.caseStatus} 
+                    </Typography>
+
+                    <Typography color="textSecondary">
+                      Tracking #: <a href="www.example.com">{card.trackId}</a>
+                    </Typography>
+                  </Paper>
+              </Grid>
+            ))}
+          </Grid>
+          </Paper>
     </div>
     );
   }
 }
+
